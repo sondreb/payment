0500588d9b8505cecdae586e482e8667b7660297
SondreB <sondre@outlook.com>
2025-01-14 23:26:45 +0100

    Add the home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CommonModule, FormsModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    standalone: true,
+    imports: [RouterOutlet],
+    template: `<router-outlet />`,
+    styles: ``
 })
-export class AppComponent implements OnInit {
-  deferredPrompt: any;
-  showInstallButton = false;
-
-  ngOnInit() {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      e.preventDefault();
-      this.deferredPrompt = e;
-      this.showInstallButton = true;
-    });
-  }
-
-  async installPwa() {
-    if (!this.deferredPrompt) return;
-    
-    this.deferredPrompt.prompt();
-    const { outcome } = await this.deferredPrompt.userChoice;
-    
-    if (outcome === 'accepted') {
-      this.showInstallButton = false;
-    }
-    this.deferredPrompt = null;
-  }
+export class AppComponent {
 }
